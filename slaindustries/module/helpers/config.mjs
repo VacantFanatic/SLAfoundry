1d82a703e7e9089aaf0e22b767ba5c9196262e1d
VacantFanatic <agill084@gmail.com>
2023-05-15 17:31:34 -0400

    Add files via upload
    initial commit

---
+++ slaindustries/module/helpers/config.mjs
@@ -0,0 +1,30 @@
+export const slaindustries = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ slaindustries.abilities = {
+  "str": "slaindustries.AbilityStr",
+  "dex": "slaindustries.AbilityDex",
+  "know": "slaindustries.AbilityKnow",
+  "conc": "slaindustries.AbilityConc",
+  "cha": "slaindustries.AbilityCha",
+  "cool": "slaindustries.AbilityCool",
+  "luck": "slaindustries.AbilityLuck",
+  "closing": "slaindustries.AbilityClosing",
+  "rush": "slaindustries.AbilityRush"
+};
+
+slaindustries.abilityAbbreviations = {
+  "str": "slaindustries.AbilityStrAbbr",
+  "dex": "slaindustries.AbilityDexAbbr",
+  "know": "slaindustries.AbilityKnowAbbr",
+  "conc": "slaindustries.AbilityConcAbbr",
+  "cha": "slaindustries.AbilityChaAbbr",
+  "cool": "slaindustries.AbilityCoolAbbr",
+  "luck": "slaindustries.AbilityLuckAbbr",
+  "closing": "slaindustries.ClosingChaAbbr",
+  "rush": "slaindustries.AbilityRushAbbr"
+  
+};
